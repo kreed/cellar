2f5c2182a6ee9124b860cd8442689b58bdd80ba3
Christopher Eby <kreed@kreed.org>
2025-10-05 19:55:38 -0700

    Remove certbot and cockpit

--- build_files/build.sh
+++ build_files/build.sh
@@ -4,12 +4,6 @@ set -ouex pipefail
 
 rpm-ostree install \
     tailscale \
-    python3-certbot \
-    python3-pip \
-    cockpit-networkmanager \
-    cockpit-podman \
-    cockpit-selinux \
-    cockpit-system \
     neovim \
     fish \
     firewalld \
@@ -17,10 +11,9 @@ rpm-ostree install \
     wget \
     golang
 
-pip3 install --prefix /usr certbot_dns_porkbun
-
 INSTALL_K3S_BIN_DIR=/usr/bin INSTALL_K3S_SKIP_ENABLE=true /ctx/k3s.sh
 
 systemctl enable tailscaled upgrader.timer k3s
 
-rm -r ~/.cache
+# see https://docs.k3s.io/installation/requirements?_highlight=firewalld&os=rhel#operating-systems
+systemctl disable firewalld
